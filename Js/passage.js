d8969fb8da093cd8ede1ea7ea3282ad0d8c2434d
YBoah <yawboahene.yb@gmail.com>
2023-10-08 12:20:29 +0000

    Added the paragraphs

---
+++ Js/passage.js
@@ -0,0 +1,18 @@
+const paragraphs =[
+    "Cristiano Ronaldo, often referred to as CR7, is a legendary footballer known for his incredible skills and goal-scoring prowess. Born in Madeira, Portugal, on February 5, 1985, Ronaldo has achieved numerous records and accolades throughout his career.",
+    "CR7 began his professional career with Sporting CP in Portugal before moving to Manchester United in 2003. During his time with the Red Devils, he won three Premier League titles and a UEFA Champions League trophy.",
+    "In 2009, Cristiano Ronaldo made a historic transfer to Real Madrid for a then-world record fee. At Real Madrid, he became the club's all-time leading scorer and won four Champions League titles.",
+    "After a successful stint in Spain, CR7 transferred to Juventus in 2018. He continued to dominate the football world, winning Serie A titles and breaking more records.",
+    "Aside from his club success, Ronaldo has been a key figure for the Portuguese national team. He has represented Portugal in multiple European Championships and FIFA World Cups, leading them to victory in Euro 2016 and the UEFA Nations League in 2019.",
+    "Known for his incredible work ethic, athleticism, and dedication to the game, Cristiano Ronaldo remains one of the greatest footballers of all time. His famous number 7 jersey is iconic in the world of football.",
+    "Off the pitch, CR7 is also a successful businessman and philanthropist. He has his own fashion line, fragrance brand, and has contributed to various charitable causes.",
+    "In summary, Cristiano Ronaldo, or CR7, is more than just a footballer. He is a global icon, a record-breaker, and an inspiration to aspiring athletes worldwide.",
+    "Lionel Messi, often regarded as the greatest footballer of all time, is a true legend of the sport. His incredible skills on the field have earned him numerous accolades and records.",
+    "Messi's dribbling ability is second to none. He can glide past defenders with ease, leaving them in his wake. His close ball control is simply mesmerizing to watch.",
+    "Scoring goals seems to come naturally to Messi. He holds the record for the most goals scored in a calendar year, a testament to his scoring prowess.",
+    "But it's not just his goals that make Messi special. His vision and passing ability are exceptional. He can pick out a teammate with pinpoint accuracy, often providing assists that lead to crucial goals.",
+    "Throughout his career, Messi has been a one-club man, spending the majority of his career at Barcelona. His loyalty to the club and its fans is truly commendable.",
+    "In 2021, Messi made a surprising move to Paris Saint-Germain (PSG), marking a new chapter in his illustrious career. Football fans around the world eagerly await his performances in the French league.",
+    "Off the field, Messi is known for his philanthropic work. He has been involved in various charitable initiatives, making a positive impact on society.",
+    "In summary, Lionel Messi's legacy in the world of football is secure. He will always be remembered as one of the greatest athletes of all time."
+];

5323c04d0cf3dce850ae3a6e5022034c390bcab7
YBoah <yawboahene.yb@gmail.com>
2023-10-09 19:08:17 +0000

    Spacing of the paragraphs

--- Js/passage.js
+++ Js/passage.js
@@ -1,36 +1,67 @@
 const paragraphs =[
     "Cristiano Ronaldo, often referred to as CR7, is a legendary footballer known for his incredible skills and goal-scoring prowess. Born in Madeira, Portugal, on February 5, 1985, Ronaldo has achieved numerous records and accolades throughout his career.",
+
     "CR7 began his professional career with Sporting CP in Portugal before moving to Manchester United in 2003. During his time with the Red Devils, he won three Premier League titles and a UEFA Champions League trophy.",
+    
     "In 2009, Cristiano Ronaldo made a historic transfer to Real Madrid for a then-world record fee. At Real Madrid, he became the club's all-time leading scorer and won four Champions League titles.",
+
     "After a successful stint in Spain, CR7 transferred to Juventus in 2018. He continued to dominate the football world, winning Serie A titles and breaking more records.",
+
     "Aside from his club success, Ronaldo has been a key figure for the Portuguese national team. He has represented Portugal in multiple European Championships and FIFA World Cups, leading them to victory in Euro 2016 and the UEFA Nations League in 2019.",
+    
     "Known for his incredible work ethic, athleticism, and dedication to the game, Cristiano Ronaldo remains one of the greatest footballers of all time. His famous number 7 jersey is iconic in the world of football.",
+
     "Off the pitch, CR7 is also a successful businessman and philanthropist. He has his own fashion line, fragrance brand, and has contributed to various charitable causes.",
+
     "In summary, Cristiano Ronaldo, or CR7, is more than just a footballer. He is a global icon, a record-breaker, and an inspiration to aspiring athletes worldwide.",
+
     "Lionel Messi, often regarded as the greatest footballer of all time, is a true legend of the sport. His incredible skills on the field have earned him numerous accolades and records.",
+
     "Messi's dribbling ability is second to none. He can glide past defenders with ease, leaving them in his wake. His close ball control is simply mesmerizing to watch.",
+
     "Scoring goals seems to come naturally to Messi. He holds the record for the most goals scored in a calendar year, a testament to his scoring prowess.",
+
     "But it's not just his goals that make Messi special. His vision and passing ability are exceptional. He can pick out a teammate with pinpoint accuracy, often providing assists that lead to crucial goals.",
+
     "Throughout his career, Messi has been a one-club man, spending the majority of his career at Barcelona. His loyalty to the club and its fans is truly commendable.",
+
     "In 2021, Messi made a surprising move to Paris Saint-Germain (PSG), marking a new chapter in his illustrious career. Football fans around the world eagerly await his performances in the French league.",
+
     "Off the field, Messi is known for his philanthropic work. He has been involved in various charitable initiatives, making a positive impact on society.",
+
     "In summary, Lionel Messi's legacy in the world of football is secure. He will always be remembered as one of the greatest athletes of all time.",
+
     "Eden Hazard, born on January 7, 1991, in La Louvière, Belgium, is a renowned professional football player known for his exceptional skills and versatility on the field.",
+
     "Hazard began his football career at a young age, playing for local clubs before joining the youth academy of Lille OSC in France.",
+
     "His breakthrough came in the 2010-2011 season when he played a key role in helping Lille win the Ligue 1 title, showcasing his dribbling ability and creative playmaking.",
+
     "In 2012, Eden Hazard signed with Chelsea FC in the English Premier League, where he became one of the club's all-time greats. During his time at Chelsea, he won multiple Premier League titles and other domestic and international trophies.",
+
     "Known for his explosive acceleration, close ball control, and vision, Hazard was often compared to some of the greatest footballers in the world.",
+
     "In 2019, Hazard signed with Real Madrid, fulfilling a lifelong dream of playing for one of the most prestigious clubs in the world. He continues to be a key player for both Real Madrid and the Belgian national team.",
     "Off the pitch, Eden Hazard is known for his humility and sportsmanship, making him a beloved figure in the football world.",
+
     "As one of the most exciting and talented players of his generation, Eden Hazard's contributions to the world of football are truly remarkable.",
+
     "Neymar Jr., whose full name is Neymar da Silva Santos Júnior, is a renowned Brazilian professional footballer. He was born on February 5, 1992, in Mogi das Cruzes, Brazil. Neymar is widely regarded as one of the most skillful and exciting players in the world of football.",
+
     "Neymar began his football journey at a young age and quickly rose through the ranks. He gained recognition for his incredible dribbling skills, creativity on the field, and goal-scoring ability. His talent didn't go unnoticed, and he soon joined the youth academy of Santos FC.",
+
     "At Santos FC, Neymar made a name for himself as one of the brightest prospects in Brazilian football. He played a crucial role in Santos winning the Copa Libertadores in 2011, and his performances drew the attention of top European clubs.",
+
     "In 2013, Neymar made a high-profile move to FC Barcelona, where he formed a formidable attacking trio with Lionel Messi and Luis Suárez. During his time at Barcelona, he won numerous titles, including La Liga and the UEFA Champions League.",
+
     "After leaving Barcelona in 2017, Neymar joined Paris Saint-Germain (PSG) in a world-record transfer deal. He continued to excel, helping PSG secure domestic titles and making significant contributions in the UEFA Champions League.",
+
     "Neymar is also a key player for the Brazilian national team. He has represented Brazil in multiple FIFA World Cup tournaments and played a pivotal role in their 2013 FIFA Confederations Cup victory and the gold medal win at the 2016 Rio Olympics.",
+
     "Off the field, Neymar is known for his philanthropic efforts and endorsements with major brands. He remains a beloved figure in the football world, known for his flashy style and exceptional skills.",
+
     "In summary, Neymar Jr. is a football icon, known for his extraordinary talent, charisma, and contributions to the sport. His journey from a young football enthusiast in Brazil to a global superstar is truly remarkable.",
+
     "Many people often misunderstand Mohammed Kudus as just a football player, but in reality, he is much more than that. He embodies the spirit of determination and hard work. His journey in the world of soccer is like a thrilling adventure, filled with ups and downs.",
     
     "In today's football landscape, Mohammed Kudus shines as a true star. His skills on the field are nothing short of magical. Watching him control the ball and make incredible passes feels like witnessing an artist at work. He is a true maestro of the game.",
